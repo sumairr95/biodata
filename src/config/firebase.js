32e62efdad11597b8b7c44f651a580f7a3bda371
sumair rana <sumairr95@gmail.com>
2023-04-02 12:29:01 -0400

    biodata

---
+++ src/config/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+
+
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD9dABDsdKbvsOievMauhi6c5WrndHpNfo",
+    authDomain: "biodatabase-bec6f.firebaseapp.com",
+    projectId: "biodatabase-bec6f",
+    storageBucket: "biodatabase-bec6f.appspot.com",
+    messagingSenderId: "957651033141",
+    appId: "1:957651033141:web:b311c981cd7509db39e57c",
+    measurementId: "G-RMCLGVW3TD"
+};
+
+
+const app = initializeApp(firebaseConfig);
